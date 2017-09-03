b7b571cd5591c5b2a75333c15f4c59e6787ba59c
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2017-09-03 22:37:56 +0530

    Initial commit

---
+++ app/src/main/java/com/nkhedekar/compass2/CompassActivity.java
@@ -0,0 +1,13 @@
+package com.nkhedekar.compass2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CompassActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+}
